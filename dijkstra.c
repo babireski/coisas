bbb39cb6c396ef50571dedbc4cdbd04c3000696c
Pedro <pedrovargastannuri@gmail.com>
2023-05-03 19:47:41 -0300

    lol cpa funfa

--- dijkstra.c
+++ dijkstra.c
@@ -8,30 +8,21 @@
 #define true 1
 #define false 0
 
-// Structure to represent a min heap node
 struct MinHeapNode
 {
 	int v;
 	int dist;
 };
 
-// Structure to represent a min heap
 struct MinHeap
 {
 	
-	// Number of heap nodes present currently
 	int size;	
-
-	// Capacity of min heap
 	int capacity;
-
-	// This is needed for decreaseKey()
 	int *pos;
 	struct MinHeapNode **array;
 };
 
-// A utility function to create a
-// new Min Heap Node
 struct MinHeapNode* newMinHeapNode(int v,
 								int dist)
 {
@@ -60,9 +51,6 @@ struct MinHeap* createMinHeap(int capacity)
 	return minHeap;
 }
 
-// A utility function to swap two
-// nodes of min heap.
-// Needed for min heapify
 void swapMinHeapNode(struct MinHeapNode** a,
 					struct MinHeapNode** b)
 {
@@ -71,11 +59,6 @@ void swapMinHeapNode(struct MinHeapNode** a,
 	*b = t;
 }
 
-// A standard function to
-// heapify at given idx
-// This function also updates
-// position of nodes when they are swapped.
-// Position is needed for decreaseKey()
 void minHeapify(struct MinHeap* minHeap,
 								int idx)
 {
@@ -114,61 +97,46 @@ void minHeapify(struct MinHeap* minHeap,
 	}
 }
 
-// A utility function to check if
-// the given minHeap is empty or not
 int isEmpty(struct MinHeap* minHeap)
 {
 	return minHeap->size == 0;
 }
 
-// Standard function to extract
-// minimum node from heap
 struct MinHeapNode* extractMin(struct MinHeap*
 								minHeap)
 {
 	if (isEmpty(minHeap))
 		return NULL;
 
-	// Store the root node
+
 	struct MinHeapNode* root =
 				minHeap->array[0];
 
-	// Replace root node with last node
 	struct MinHeapNode* lastNode =
 		minHeap->array[minHeap->size - 1];
 	minHeap->array[0] = lastNode;
 
-	// Update position of last node
 	minHeap->pos[root->v] = minHeap->size-1;
 	minHeap->pos[lastNode->v] = 0;
 
-	// Reduce heap size and heapify root
 	--minHeap->size;
 	minHeapify(minHeap, 0);
 
 	return root;
 }
 
-// Function to decreasekey dist value
-// of a given vertex v. This function
-// uses pos[] of min heap to get the
-// current index of node in min heap
+
 void decreaseKey(struct MinHeap* minHeap,
 						int v, int dist)
 {
-	// Get the index of v in heap array
+
 	int i = minHeap->pos[v];
 
-	// Get the node and update its dist value
 	minHeap->array[i]->dist = dist;
 
-	// Travel up while the complete
-	// tree is not heapified.
-	// This is a O(Logn) loop
 	while (i && minHeap->array[i]->dist <
 		minHeap->array[(i - 1) / 2]->dist)
 	{
-		// Swap this node with its parent
 		minHeap->pos[minHeap->array[i]->v] =
 									(i-1)/2;
 		minHeap->pos[minHeap->array[
@@ -176,13 +144,10 @@ void decreaseKey(struct MinHeap* minHeap,
 		swapMinHeapNode(&minHeap->array[i],
 				&minHeap->array[(i - 1) / 2]);
 
-		// move to parent index
 		i = (i - 1) / 2;
 	}
 }
 
-// A utility function to check if a given vertex
-// 'v' is in min heap or not
 int isInMinHeap(struct MinHeap *minHeap, int v)
 {
 if (minHeap->pos[v] < minHeap->size)
@@ -190,7 +155,6 @@ if (minHeap->pos[v] < minHeap->size)
 return false;
 }
 
-// A utility function used to print the solution
 void printArr(int dist[], int n)
 {
 	printf("Vertex Distance from Source\n");
@@ -198,24 +162,14 @@ void printArr(int dist[], int n)
 		printf("%d \t\t %d\n", i, dist[i]);
 }
 
-// The main function that calculates
-// distances of shortest paths from src to all
-// vertices. It is a O(ELogV) function
 void dijkstra(struct graph* graph, int src)
 {
 	
-	// Get the number of vertices in graph
 	int V = graph->order;
-
-	// dist values used to pick
-	// minimum weight edge in cut
 	int dist[V];	
 
-	// minHeap represents set E
 	struct MinHeap* minHeap = createMinHeap(V);
 
-	// Initialize min heap with all
-	// vertices. dist value of all vertices
 	for (int v = 0; v < V; ++v)
 	{
 		dist[v] = INT_MAX;
@@ -223,85 +177,50 @@ void dijkstra(struct graph* graph, int src)
 									dist[v]);
 		minHeap->pos[v] = v;
 	}
-
-	// Make dist value of src vertex
-	// as 0 so that it is extracted first
 	minHeap->array[src] =
 		newMinHeapNode(src, dist[src]);
 	minHeap->pos[src] = src;
 	dist[src] = 0;
 	decreaseKey(minHeap, src, dist[src]);
-
-	// Initially size of min heap is equal to V
 	minHeap->size = V;
 
-	// In the following loop,
-	// min heap contains all nodes
-	// whose shortest distance
-	// is not yet finalized.
 	while (!isEmpty(minHeap))
 	{
-		// Extract the vertex with
-		// minimum distance value
+
 		struct MinHeapNode* minHeapNode =
 					extractMin(minHeap);
 	
-		// Store the extracted vertex number
 		int u = minHeapNode->v;
 
-		// Traverse through all adjacent
-		// vertices of u (the extracted
-		// vertex) and update
-		// their distance values
 		struct edge *pCrawl =
 					graph->nodes[u].first;
 		while (pCrawl != NULL)
 		{
 			int v = pCrawl->node;
-
-			// If shortest distance to v is
-			// not finalized yet, and distance to v
-			// through u is less than its
-			// previously calculated distance
 			if (isInMinHeap(minHeap, v) &&
 					dist[u] != INT_MAX &&
 			pCrawl->weight + dist[u] < dist[v])
 			{
 				dist[v] = dist[u] + pCrawl->weight;
 
-				// update distance
-				// value in min heap also
 				decreaseKey(minHeap, v, dist[v]);
 			}
 			pCrawl = pCrawl->next;
 		}
 	}
 
-	// print the calculated shortest distances
 	printArr(dist, V);
 }
 
 
-// Driver program to test above functions
+
 int main()
 {
-	// create the graph given in above figure
-	int V = 9;
+	int V = 3;
 	struct graph* graph = create(V);
-	addEdge(graph, 0, 1, 4);
-	addEdge(graph, 0, 7, 8);
-	addEdge(graph, 1, 2, 8);
-	addEdge(graph, 1, 7, 11);
-	addEdge(graph, 2, 3, 7);
-	addEdge(graph, 2, 8, 2);
-	addEdge(graph, 2, 5, 4);
-	addEdge(graph, 3, 4, 9);
-	addEdge(graph, 3, 5, 14);
-	addEdge(graph, 4, 5, 10);
-	addEdge(graph, 5, 6, 2);
-	addEdge(graph, 6, 7, 1);
-	addEdge(graph, 6, 8, 6);
-	addEdge(graph, 7, 8, 7);
+	addEdge(graph, 4, 0, 2);
+	addEdge(graph, 2, 0, 1);
+	addEdge(graph, 1, 1, 2);
 
 	dijkstra(graph, 0);
 
--- graph.c
+++ graph.c
@@ -14,7 +14,7 @@ void addNode(graph *descriptor, void* data)
 {
 	descriptor -> order += 1;
 	descriptor -> nodes = (node *) realloc(descriptor -> nodes, descriptor -> order * sizeof(node));
-	//descriptor -> nodes[descriptor -> order - 1].data = data;
+	descriptor -> nodes[descriptor -> order - 1].data = data;
 	descriptor -> nodes[descriptor -> order - 1].degree = 0;
 	descriptor -> nodes[descriptor -> order - 1].first = NULL;
 }
