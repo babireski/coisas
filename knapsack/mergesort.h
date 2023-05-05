f4b6747a5a53a3baff8f41ece3cdc98136286edb
babireski <elian.babireski@gmail.com>
2023-05-05 19:42:47 -0300

    Fazer o mergesort genérico vai dar trabalho

--- knapsack/mergesort.h
+++ knapsack/mergesort.h
@@ -1,4 +1,9 @@
 #ifndef _mergesort_h_
 	#define _mergesort_h_
-	void mergesort(void* array, int n, int m, int (*compare)(const void*, const void*));
+
+	#include <stdlib.h>
+
+	void mergesort(void* array, int n, int t, int (*compare)(const void*, const void*));
+	void split(void* array, int l, int r, int t, int (*compare)(const void*, const void*));
+	void merge(void* array, int l, int h, int r, int t, int (*compare)(const void*, const void*));
 #endif
