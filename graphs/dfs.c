95ce25ca270a8ce3e2569ec820350e50063c53f7
babireski <elian.babireski@gmail.com>
2023-05-05 15:52:16 -0300

    Comeaçndo a organizar

--- graphs/dfs.c
+++ graphs/dfs.c
@@ -1,17 +1,18 @@
 #include "dfs.h"
-void DFS(struct graph* graph, int vertex, int* visited) {
-  struct edge* adjList = graph->nodes[vertex].first;
-  struct edge* temp = adjList;
 
-  visited[vertex] = 1;
-  printf("Visited %s \n", (char*) graph->nodes[vertex].data);
+void dfs(struct graph* graph, int source, int* visited)
+{
+	edge* adjList = graph -> nodes[source].first;
+	edge* temp = adjList;
 
-  while (temp != NULL) {
-    int connectedVertex = temp->node;
+	visited[source] = 1;
+	printf("Visited %s \n", (char*) graph -> nodes[source].data);
 
-    if (visited[connectedVertex] == 0) {
-      DFS(graph, connectedVertex, visited);
-    }
-    temp = temp->next;
+	while(temp != NULL)
+	{
+		int connectedVertex = temp->node;
+		if(visited[connectedVertex] == 0)
+			dfs(graph, connectedVertex, visited);
+		temp = temp -> next;
   }
 }
