5056896cf86eaf7d5d3a876f914615d9d556885c
Pedro <pedrovargastannuri@gmail.com>
2023-06-08 13:42:06 -0300

    sai pra la arquivos bonarios

--- cortesDeBarra.c
+++ cortesDeBarra.c
@@ -6,10 +6,10 @@ int *S;
 int p[TAM+1] = {0, 1, 3, 11, 16, 19, 10};
 void imprimeCortes(int n)
 {
-    for (int i = n - 1; i >= 0; --i)
-    {
-        printf("%d ", S[i]);
-    }
+	for (int i = n - 1; i >= 0; --i)
+	{
+		printf("%d ", S[i]);
+	}
 }
 int cBRecTopDown(int n)
 {
