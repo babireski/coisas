eefe4f542398b21eb688ea5f4a7b5949dcac1330
Pedro <pedrovargastannuri@gmail.com>
2023-06-09 12:56:57 -0300

    ok

--- scheduling/scheduling.cpp
+++ scheduling/scheduling.cpp
@@ -1,40 +1,51 @@
 #include <iostream>
 #include "train.hpp"
 #include <vector>
-
+using namespace std;
 int main()
 {
 	// n -> nro de plataformas ; m -> nro de trens
 	int n, m;
-	std::vector<train> trains;
-	std::cout << "Digite o numero de trens e de plataformas\n";
-	std::cin >> n >> m;
-	trains.resize(n);
+	vector<train> trains;
+	cout << "Digite o numero de plataformas\n";
+	cin >> n;
+	cout << "Digite o numero de trens\n";
+	cin >> m;
+	trains.resize(m);
 	// initialize the trains
-	for(int i = 0; i < m; i++)
+	for (int i = 0; i < m; i++)
 	{
-		int h,m,p;
-		std::cout << "Digite a plataforma do trem\n";
-		std::cin >> p;
+		int h, m, p;
+		cout << "Digite a plataforma do trem\n";
+		cin >> p;
 		tTime tim;
 		trains[i].platform = p;
-		if(p > 3 || p < 1)
+		if (p > 3 || p < 1)
 		{
 			i--;
-			std::cout << "Erro, digite uma plataforma valida\n";
+			cout << "Erro, digite uma plataforma valida\n";
 		}
 		else
 		{
-			std::cout << "Digite a hora e o minuto de chegada do trem, separados por espaco\n";
-			std::cin >> h >> m;
-			if(tim.isValid(h,m))
+			cout << "Digite a hora e o minuto de chegada do trem, separados por espaco\n";
+			cin >> h >> m;
+			if (tim.isValid(h, m))
 			{
 				tim.hour = h;
 				tim.minutes = m;
 				trains[i].arrival = tim;
 			}
-			std::cout << trains[i].print();
+			cout << "Digite a hora e o minuto de partida do trem, separados por espaco\n";
+			cin >> h >> m;
+			if (tim.isValid(h, m))
+			{
+				tim.hour = h;
+				tim.minutes = m;
+				trains[i].departure = tim;
+			}
+		
 		}
 	}
-
+	trains.clear();
+	return 0;
 }
