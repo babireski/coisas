8344ce74fd887a6ddbd4972f4fb5d8a4241a5136
Pedro <pedrovargastannuri@gmail.com>
2023-06-10 08:09:30 -0300

    mochila fracionaria

--- knapsack/mergesort.h
+++
@@ -1,9 +0,0 @@
-#ifndef _mergesort_h_
-	#define _mergesort_h_
-
-	#include <stdlib.h>
-
-	void mergesort(void* array, int n, int t, int (*compare)(const void*, const void*));
-	void split(void* array, int l, int r, int t, int (*compare)(const void*, const void*));
-	void merge(void* array, int l, int h, int r, int t, int (*compare)(const void*, const void*));
-#endif
